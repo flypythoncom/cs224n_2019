8baa4f6bf97d6f39391d9abed88aa22266d47f49
chongjiu.jin <chongjiu.jin@zoom.us>
2019-12-03 17:36:23 +0800

    modify test

--- Assignment_origin/Assignment 5/cnn.py
+++ Assignment_origin/Assignment 5/cnn.py
@@ -6,6 +6,23 @@ CS224N 2018-19: Homework 5
 """
 
 ### YOUR CODE HERE for part 1i
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+class CNN(nn.Module):
+    def __init__(self, in_ch, out_ch,k=5):
+        """ 
+        Apply the output of the convolution later (x_conv) through a highway network
+                @param D_in (int): Size of input layer 
+                @param H (int): Size of Hidden layer
+                @param D_out (int): Size of output layer
+                @param prob (float): Probability of dropout
+        """
+        super(CNN, self).__init__()
+     
 
+
+    def forward(self, x):
+        pass
 ### END YOUR CODE
 
--- Assignment_origin/Assignment 5/highway.py
+++ Assignment_origin/Assignment 5/highway.py
@@ -6,7 +6,27 @@ CS224N 2018-19: Homework 5
 """
 
 ### YOUR CODE HERE for part 1h
+import torch
+import numpy as np
+class Highway(torch.nn.Module):
+    def __init__(self, D_in, H, D_out,prob):
+        """
+        Apply the output of the convolution later (x_conv) through a highway network
+                @param D_in (int): Size of input layer
+                @param H (int): Size of Hidden layer
+                @param D_out (int): Size of output layer
+                @param prob (float): Probability of dropout
+        """
+        super(Highway, self).__init__()
 
+
+    def forward(self, x):
+        """
+        Apply the output of the convolution later (x_conv) through a highway network
+                @param x (Tensor): Input x_cov gets applied to Highway network - shape of input tensor [batch_size,1,e_word]
+                @returns x_pred (Tensor): Size of Hidden layer -- NOTE: check the shapes
+        """
+        pass
 
 
 ### END YOUR CODE 
